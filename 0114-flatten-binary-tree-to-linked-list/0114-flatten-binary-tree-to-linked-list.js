a356ca2b2bc0b323acc2675171750dc89e9a2df6
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-08-13 20:12:49 +0100

    Time: 0 ms (100%), Space: 56.8 MB (73.79%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    if (root === null) return null;
+
+    const stack = [root];
+    let previous = null
+
+    while (stack.length) {
+        const current = stack.pop()
+
+        if (current.right) stack.push(current.right);
+        if (current.left) stack.push(current.left);
+
+        current.right = null;
+        current.left = null;
+
+        if (previous) previous.right = current;
+        previous = current;
+    }
+
+    return root;
+};
