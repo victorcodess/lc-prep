efe65edb3187987d47a7f08726f53d0c1f200ecf
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-08-24 16:47:52 +0100

    Time: 11 ms (30.99%), Space: 66.6 MB (20.01%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) { // Time: O(nlogn), Space: O(n)
+    intervals.sort((a, b) => a[0] - b[0]);
+    const stack = [intervals[0]];
+    let i = 1;
+    const result = [];
+
+    while (i < intervals.length) {
+        const [prevS, prevE] = stack[stack.length - 1];
+        const [currS, currE] = intervals[i];
+        const minS = Math.min(currS, prevS);
+        const maxS = Math.max(currS, prevS);
+        const minE = Math.min(currE, prevE);
+        const maxE = Math.max(currE, prevE);
+
+        const final = intervals[i];
+
+        if ((minE - maxS) >= 0) {
+            stack.pop();
+            stack.push([minS, maxE]);
+        } else {
+            stack.push(intervals[i]);
+        }
+
+        i++;
+    }
+
+    return stack;
+};
