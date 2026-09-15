b41c40b750bc98033026a3a7dd1bf36f59aea203
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-15 16:10:33 +0100

    Time: 10 ms (77.05%), Space: 63.2 MB (24.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -3,26 +3,55 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) { // Time: O(nlog(n)), Space: O(n)
+// var topKFrequent = function(nums, k) { // Time: O(nlog(k)), Space: O(n)
+//     const freqMap = new Map();
+//     const minHeap = new MinPriorityQueue((num) => num[1]);
+
+//     for (let num of nums) {
+//         freqMap.set(num, (freqMap.get(num) || 0) + 1);
+//     }
+
+//     for (let [val, freq] of freqMap) {
+//         minHeap.enqueue([val, freq]);
+
+//         if (minHeap.size() > k) {
+//             minHeap.dequeue();
+//         }
+//     }
+
+//     const result = [];
+
+//     while (minHeap.size() > 0) {
+//         result.push(minHeap.dequeue()[0]);
+//     }
+
+//     return result;
+// };
+
+var topKFrequent = function(nums, k) { // Time: O(n), Space: O(n)
     const freqMap = new Map();
-    const minHeap = new MinPriorityQueue((num) => num[1]);
 
     for (let num of nums) {
         freqMap.set(num, (freqMap.get(num) || 0) + 1);
     }
 
-    for (let [val, freq] of freqMap) {
-        minHeap.enqueue([val, freq]);
+    const buckets = new Array(nums.length + 1).fill(null).map(() => []);
 
-        if (minHeap.size() > k) {
-            minHeap.dequeue();
-        }
+    for (let [val, freq] of freqMap) {
+       buckets[freq].push(val);
     }
 
     const result = [];
 
-    while (minHeap.size() > 0) {
-        result.push(minHeap.dequeue()[0]);
+    for (let i = buckets.length - 1; i >= 0; i--) {
+        const bucket = buckets[i];
+
+        if (k <= 0) break;
+
+        for (let num of buckets[i]) {
+            result.push(num);
+            k--;
+        }
     }
 
     return result;
