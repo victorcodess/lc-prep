854e0e05a7c1b23f3ba26a7710717fe150514a58
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-15 14:16:21 +0100

    Time: 3 ms (78.27%), Space: 63 MB (15.84%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -2,34 +2,57 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// var sortedSquares = function(nums) { // Time: O(n), Space: O(n)
+//     const result = [];
+
+//     let closestToZero = [0, Infinity];
+
+//     for (let k = 0; k < nums.length; k++) {
+//         if (Math.abs(nums[k] - 0) <= closestToZero[1]) {
+//             closestToZero = [k, Math.abs(nums[k])];
+//         } else {
+//             break;
+//         }
+//     }
+
+//     let i = closestToZero[0] - 1;
+//     let j = closestToZero[0];
+
+//     console.log(i, j)
+
+//     while (i >= 0 || j < nums.length) {
+//         const a = i >= 0 ? Math.abs(nums[i]) : Infinity;
+//         const b = j < nums.length ? Math.abs(nums[j]) : Infinity;
+
+//         if (a < b) {
+//             result.push(a * a);
+//             i--;
+//         } else {
+//             result.push(b * b);
+//             j++;
+//         }
+//     }
+
+//     return result;
+// };
+
 var sortedSquares = function(nums) { // Time: O(n), Space: O(n)
-    const result = [];
+    const result = new Array(nums.length).fill(0);
 
-    let closestToZero = [0, Infinity];
+    let i = 0;
+    let j = nums.length - 1;
+    let pos = result.length - 1;
 
-    for (let k = 0; k < nums.length; k++) {
-        if (Math.abs(nums[k] - 0) <= closestToZero[1]) {
-            closestToZero = [k, Math.abs(nums[k])];
+    while (i <= j) {
+        const a = Math.abs(nums[i]);
+        const b = Math.abs(nums[j]);
+
+        if (a > b) {
+            result[pos--] = a * a;
+            i++;
         } else {
-            break;
-        }
-    }
-
-    let i = closestToZero[0] - 1;
-    let j = closestToZero[0];
-
-    console.log(i, j)
-
-    while (i >= 0 || j < nums.length) {
-        const a = i >= 0 ? Math.abs(nums[i]) : Infinity;
-        const b = j < nums.length ? Math.abs(nums[j]) : Infinity;
-
-        if (a < b) {
-            result.push(a * a);
-            i--;
-        } else {
-            result.push(b * b);
-            j++;
+            result[pos--] = b * b;
+            j--;
         }
     }
 
