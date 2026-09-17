057bc15bb1c6e31a2c453bc4c33574b60b61b951
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-17 23:24:36 +0100

    Time: 0 ms (100%), Space: 55.8 MB (57.38%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.js
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.js
@@ -7,12 +7,9 @@ var missingMultiple = function(nums, k) { // Time: O(n), Space: O(n)
     let n = 1;
     const numSet = new Set(nums);
 
-    while (n > 0) {
-        const multiple = n * k;
-        if (!numSet.has(multiple)) {
-            return multiple;
-        }
-
+    while (numSet.has(n * k)) {
         n++;
     }
+
+    return n * k;
 };
