30effe03b3196fc9f964aa425170735c971bab42
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-18 12:36:45 +0100

    Time: 1 ms (63.86%), Space: 55.1 MB (66.96%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -2,29 +2,51 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) { // Time: O(n), Space: O(n)
-    const maxLefts = new Array(height.length).fill(0);
-    const maxRights = new Array(height.length).fill(0);
+// var trap = function(height) { // Time: O(n), Space: O(n)
+//     const maxLefts = new Array(height.length).fill(0);
+//     const maxRights = new Array(height.length).fill(0);
 
-    let maxL = 0;
-    let maxR = 0;
+//     let maxL = 0;
+//     let maxR = 0;
 
-    for (let i = 0; i < height.length; i++) {
-        maxLefts[i] = maxL;
-        maxL = Math.max(maxL, height[i]);
-    }
+//     for (let i = 0; i < height.length; i++) {
+//         maxLefts[i] = maxL;
+//         maxL = Math.max(maxL, height[i]);
+//     }
 
-    for (let i = height.length - 1; i >= 0; i--) {
-        maxRights[i] = maxR;
-        maxR = Math.max(maxR, height[i]);
-    }
+//     for (let i = height.length - 1; i >= 0; i--) {
+//         maxRights[i] = maxR;
+//         maxR = Math.max(maxR, height[i]);
+//     }
 
+//     let totalWater = 0;
+
+//     for (let i = 0; i < height.length; i++) {
+//         const water = Math.min(maxLefts[i], maxRights[i]) - height[i];
+
+//         if (water > 0) totalWater += water;
+//     }
+
+//     return totalWater;
+// };
+
+var trap = function(height) { // Time: O(n), Space: O(1)
     let totalWater = 0;
+    let l = 0;
+    let r = height.length - 1;
+    let maxL = height[l];
+    let maxR = height[r];
 
-    for (let i = 0; i < height.length; i++) {
-        const water = Math.min(maxLefts[i], maxRights[i]) - height[i];
-
-        if (water > 0) totalWater += water;
+    while (l < r) {
+        if (maxL < maxR) {
+            l++;
+            maxL = Math.max(maxL, height[l]);
+            totalWater += maxL - height[l];
+        } else {
+            r--;
+            maxR = Math.max(maxR, height[r]);
+            totalWater += maxR - height[r];
+        }
     }
 
     return totalWater;
