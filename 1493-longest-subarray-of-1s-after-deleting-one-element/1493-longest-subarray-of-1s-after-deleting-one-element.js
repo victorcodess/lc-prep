a5a1f6d87c3941411816ed79be49c30dbb4a5b6d
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-21 14:34:37 +0100

    Time: 8 ms (13.41%), Space: 62.2 MB (9.76%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function(nums) {
+    // 0,3,0,2,0,1
+    const compound = [];
+
+    let i = 0;
+
+    while (i < nums.length) {
+        if (nums[i] === 0) {
+            compound.push(nums[i]);
+            i++;
+        } else {
+            let sum = 0;
+            while (nums[i] === 1) {
+                sum += nums[i];
+                i++;
+            }
+            compound.push(sum);
+        }
+    }
+
+    let maxSub = -Infinity;
+
+    for (let i = 0; i < compound.length; i++) {
+        if (compound[i] === 0) {
+            const left = compound[i - 1] ?? 0;
+            const right = compound[i + 1] ?? 0;
+            maxSub = Math.max(maxSub, left + right);
+        }
+    }
+
+// 1,1 - > -1
+// 0,0 - > 0
+    // console.log(compound)
+
+    if (maxSub === -Infinity) {
+        return compound[0] - 1;
+    } else {
+        return maxSub;
+    }
+};
