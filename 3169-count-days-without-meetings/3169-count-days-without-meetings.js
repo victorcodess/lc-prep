4e587633fe9520d4e678c3301d6eb42f3944e92c
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-21 16:02:50 +0100

    Time: 94 ms (21.43%), Space: 83.8 MB (59.52%) - LeetHub

---
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} days
+ * @param {number[][]} meetings
+ * @return {number}
+ */
+var countDays = function(days, meetings) { // Time: O(nlog(n)), Space: O(1)
+   meetings.sort((a, b) => a[0] - b[0]);
+   let noMeet = 0;
+   let prevE = 1;
+ console.log(meetings)
+   for (let [start, end] of meetings) {
+        const diff = Math.max(0, start - prevE);
+
+        noMeet += diff;
+        
+        prevE = Math.max(prevE, end + 1);
+   }
+
+   noMeet += Math.max(0, days - (prevE - 1));
+
+   return noMeet;
+};
