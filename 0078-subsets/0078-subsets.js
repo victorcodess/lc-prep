d77ab1b80d40ad5237e2c2f313fba04255219ee4
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-21 20:32:54 +0100

    Time: 0 ms (100%), Space: 55.2 MB (42.92%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -2,18 +2,42 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsets = function(nums, i = 0) { // Time: O(c^n), Space: O(c^n)
-    if (i >= nums.length) return [[]];
+// var subsets = function(nums, i = 0) { // Time: O(n * 2^n), Space: O(n * 2^n)
+//     if (i >= nums.length) return [[]];
 
-    const first = nums[i];
+//     const first = nums[i];
 
-    const withoutFirst = subsets(nums, i + 1);
+//     const withoutFirst = subsets(nums, i + 1);
 
-    const withFirst = [];
+//     const withFirst = [];
 
-    for (let sub of withoutFirst) {
-        withFirst.push([first, ...sub]);
-    }
+//     for (let sub of withoutFirst) {
+//         withFirst.push([first, ...sub]);
+//     }
 
-    return [...withFirst, ...withoutFirst];
+//     return [...withFirst, ...withoutFirst];
+// };
+
+var subsets = function(nums) { // Time: O(n * 2^n), Space: O(n * 2^n)
+   const result = [];
+   const subs = [];
+
+   function backtrack(i) {
+        if (i >= nums.length) {
+            result.push([...subs]);
+            return;
+        }
+        
+        subs.push(nums[i]);
+
+        backtrack(i + 1);
+
+        subs.pop();
+
+        backtrack(i + 1);
+   }
+   
+   backtrack(0);
+
+   return result;
 };
