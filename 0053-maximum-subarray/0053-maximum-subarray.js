86504a96a67bc57a768398a2b60fa9345f7d41ca
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-21 22:06:43 +0100

    Time: 2 ms (70.13%), Space: 64 MB (44.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,68 +2,64 @@
  * @param {number[]} nums
  * @return {number}
  */
-// var maxSubArray = function(nums) {
-//     let prevMin = nums[0];
-//     let prevMax = nums[0];
-//     let result = nums[0];
-
-//     for (let num of nums) {
-//         let tempPMin = prevMin;
-//         let tempPMax = prevMax;
-
-//         prevMin = Math.min(prevMin, num)
-//     }
-    
-// };
-
 var maxSubArray = function(nums) { // Time: O(n), Space: O(n)
-    const memo = new Map();
+    let currSum = nums[0];
+    let result = nums[0];
 
-    function dfs(i) {
-        if (i >= nums.length) return 0;
-        if (memo.has(i)) return memo.get(i);
+    for (let i = 1; i < nums.length; i++) {
+        currSum = Math.max(nums[i], nums[i] + currSum);
 
-        const take = nums[i] + dfs(i + 1);
-        const stop = nums[i];
-
-        const result = Math.max(take, stop);
-
-        memo.set(i, result);
-        return result;
+        result = Math.max(currSum, result);
     }
-
-    let maxSub = -Infinity;
-
-    for (let i = 0; i < nums.length; i++) {
-        maxSub = Math.max(maxSub, dfs(i));
-    }
-
-    return maxSub;
+    
+    return result;
 };
 
-// var maxSubArray = function(nums, i = 0, taken = false, skipped = false, memo = new Map()) { // Time: O(n^3), Space: O(n^3)
-//     if (i >= nums.length) return 0;
+// var maxSubArray = function(nums) { // Time: O(n), Space: O(n)
+//     const memo = new Map();
 
-//     const key = i + "," + taken + "," + skipped;
+//     function dfs(i) {
+//         if (i >= nums.length) return 0;
+//         if (memo.has(i)) return memo.get(i);
+
+//         const take = nums[i] + dfs(i + 1);
+//         const stop = nums[i];
+
+//         const result = Math.max(take, stop);
+
+//         memo.set(i, result);
+//         return result;
+//     }
+
+//     let maxSub = -Infinity;
+
+//     for (let i = 0; i < nums.length; i++) {
+//         maxSub = Math.max(maxSub, dfs(i));
+//     }
+
+//     return maxSub;
+// };
+
+
+
+// DOESN'T WORK
+
+// var maxSubArray = function(nums, i = 0, taken = false, memo = new Map()) { // Time: O(n), Space: O(n)
+//     if (i >= nums.length) return taken ? -Infinity : 0;
+
+//     const key = i + "," + taken;
 //     if (memo.has(key)) return memo.get(key);
 
 //     if (taken === true) {
-//         if (skipped === true) {
-//             const skip = maxSubArray(nums, i + 1, true, true, memo);
+//         const take = nums[i] + maxSubArray(nums, i + 1, true, memo);
+//         const stop = nums[i];
+//         const result = Math.max(take, stop);
 
-//             memo.set(key, skip);
-//             return skip;
-//         } else {
-//             const take = nums[i] + maxSubArray(nums, i + 1, true, false, memo);
-//             const skip = maxSubArray(nums, i + 1, true, true, memo);
-//             const result = Math.max(take, skip);
-
-//             memo.set(key, result);
-//             return result;
-//         }
+//         memo.set(key, result);
+//         return result;
 //     } else {
-//         const take = nums[i] + maxSubArray(nums, i + 1, true, false, memo);
-//         const skip = maxSubArray(nums, i + 1, false, true, memo);
+//         const take = nums[i] + maxSubArray(nums, i + 1, true, memo);
+//         const skip = maxSubArray(nums, i + 1, false, memo);
 //         const result = Math.max(take, skip);
 
 //         memo.set(key, result);
