d586137457da55a05ad46ccec5ba53f8b7cb01ca
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-26 17:02:53 +0100

    Time: 77 ms (8.05%), Space: 100.3 MB (5.07%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -3,23 +3,44 @@
  * @param {number} k
  * @return {boolean}
  */
-var containsNearbyDuplicate = function(nums, k) { // Time: O(n * m), Space: (n)
+// var containsNearbyDuplicate = function(nums, k) { // Time: O(n * m), Space: (d)
+//     const indices = new Map();
+
+//     for (let i = 0; i < nums.length; i++) {
+//         const key = String(nums[i]);
+
+//         if (indices.has(key)) {
+//             for (let idx of indices.get(key)) {
+//                 if (Math.abs(idx - i) <= k) {
+//                     return true;
+//                 }
+//             }
+//         } else {
+//             indices.set(key, []);
+//         }
+
+//         indices.get(key).push(i);
+//     }
+
+//     return false;
+// };
+
+var containsNearbyDuplicate = function(nums, k) { // Time: O(n), Space: (n)
     const indices = new Map();
 
     for (let i = 0; i < nums.length; i++) {
         const key = String(nums[i]);
 
         if (indices.has(key)) {
-            for (let idx of indices.get(key)) {
-                if (Math.abs(idx - i) <= k) {
-                    return true;
-                }
-            }
-        } else {
-            indices.set(key, []);
-        }
+            const idx = indices.get(key);
 
-        indices.get(key).push(i);
+            if (Math.abs(idx - i) <= k) {
+                return true;
+            }
+
+        } 
+
+        indices.set(key, i);
     }
 
     return false;
