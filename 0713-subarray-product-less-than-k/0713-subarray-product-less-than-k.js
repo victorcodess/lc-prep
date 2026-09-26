cd631a025847461ee131efcefc717ae1d74c0e5c
Victor Williams <victor.williams@stu.cu.edu.ng>
2026-09-26 16:29:12 +0100

    Time: 1 ms (96.19%), Space: 59.1 MB (34.75%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.js
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.js
@@ -4,26 +4,25 @@
  * @return {number}
  */
 var numSubarrayProductLessThanK = function(nums, k) { // Time: O(n), Space: O(1)
+    if (k <= 1) return 0;
+
     let count = 0;
     let currP = 1;
 
     let i = 0;
     let j = 0;
 
-    while (i < nums.length && j < nums.length) {
-        const num = nums[j];
-        currP *= num;
+    while (j < nums.length) {
+        currP *= nums[j];
 
         while (currP >= k) {
-            const prev = nums[i];
-            currP /= prev;
+            currP /= nums[i];
             i++;
         }
 
-        if (currP < k) {
-            count += Math.max(0, (j - i + 1));
-            j++;
-        }
+        count += (j - i + 1);
+        j++;
+
     }
 
     return count;
